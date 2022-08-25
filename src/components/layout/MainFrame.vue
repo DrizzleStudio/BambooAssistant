d9081750676ae46ea592d987800911590611372b
PEI_ASUS <pei.ww@qq.com>
2022-08-25 23:18:22 +0800

    初始化项目

---
+++ src/components/layout/MainFrame.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="main-frame flex-box flex-column fill-height">
+    <div class="top-area" style="background-color: #7594af">
+      top
+      <slot name="topBar"></slot>
+    </div>
+    <div class="middle-area flex-box fill-height">
+      <div class="left-bar" style="background-color: #c49fdc">
+        left
+      </div>
+      <div class="main-area fill-width">
+        <splitpanes class="default-theme" horizontal style="height: 100%">
+          <pane>
+            <splitpanes>
+              <pane size="10">1</pane>
+              <pane>
+                <Demo></Demo>
+                <TableDemo></TableDemo>
+              </pane>
+              <pane size="10">3</pane>
+            </splitpanes>
+          </pane>
+          <pane size="10">4</pane>
+        </splitpanes>
+      </div>
+      <div class="right-bar" style="background-color: #c2a1b8">
+        right
+      </div>
+    </div>
+    <div class="bottom-area" style="background-color: #e388a3">
+      bottom
+      <slot name="buttonArea"></slot>
+    </div>
+    <div class="info-area" style="background-color: #aee7b3">
+      info
+      <!--    <slot name="infoArea"></slot>-->
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Demo from '/src/components/Demo.vue'
+import TableDemo from '/src/components/TableDemo.vue'
+
+
+</script>
+
+<style scoped>
+
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
-import { createApp } from "vue";
-import "./style.css";
+import {createApp} from "vue";
 import App from "./App.vue";
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+// @ts-ignore
+import {Splitpanes, Pane} from 'splitpanes';
+import 'splitpanes/dist/splitpanes.css';
+import '@wangeditor/editor/dist/css/style.css'
+import 'xe-utils'
+import VXETable from 'vxe-table'
+import 'vxe-table/lib/style.css'
 
-createApp(App).mount("#app");
+import './style/style.css'
+
+
+const app = createApp(App);
+
+app.component("Splitpanes", Splitpanes);
+app.component("Pane", Pane);
+
+app.use(ElementPlus, {size: 'small'})
+    .use(VXETable)
+    .mount("#app");
